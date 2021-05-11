a3f72441a84cf9afb9058b29141d5c3ecbc75dd1
Jesper Tjørnelund <69388371+tjorne@users.noreply.github.com>
2021-05-11 14:52:53 +0200

    Scoreboard now sorting by kills

--- Assets/Resources/Scripts/UI/ScoreDisplayHandler.cs
+++ Assets/Resources/Scripts/UI/ScoreDisplayHandler.cs
@@ -47,39 +47,45 @@ public class ScoreDisplayHandler : MonoBehaviour
 
     private void SetScoreboardActive(bool v) => m_scoreboard.SetActive(v);
 
+    /// <summary>
+    /// There are many ways this method could be improved!
+    ///     o The Dictionary "m_playerUIStats" might not really be necessary
+    ///     o The myList of the Dictionary screams that both data types are poor choices. 
+    ///     o Finding the player variable when looping through kvp's in myList defeats the purpose of having the key in the first place. 
+    /// </summary>
     private void UpdateScoreboard()
     {
         m_numberOfPlayers.text = PhotonNetwork.PlayerList.Length + " / 20";
 
         var yPos = 0;
 
+        foreach (var player in PhotonNetwork.PlayerList)
+        {
+            if (m_playerUIStats.ContainsKey(player.ActorNumber)) continue;
+            
+            Debug.Log("Player not seen on Scoreboard before. Creating new PlayerUIStats obj for it...");
+            var s = Instantiate(m_playerUIStatsObject, m_playerUIStatsObject.transform.position, Quaternion.identity, m_scoreboard.transform);
+            m_playerUIStats.Add(player.ActorNumber, s.GetComponent<PlayerUIStats>());
+        }
+        
         var myList = m_playerUIStats.ToList();
 
         myList.Sort((pair1, pair2) => pair1.Value.PlayerKills.text.CompareTo(pair2.Value.PlayerKills.text));
+        myList.Reverse();
 
-        foreach (var player in PhotonNetwork.PlayerList)
+        foreach (var kvp in myList)
         {
-            if (m_playerUIStats.ContainsKey(player.ActorNumber))
-            {
-                var playerUIStats = m_playerUIStats[player.ActorNumber];
+            var player = PhotonNetwork.PlayerList.Where(x => x.ActorNumber == kvp.Key).FirstOrDefault();
+            
+            kvp.Value.PlayerName.text = player.NickName;
+            kvp.Value.PlayerKills.text = player.CustomProperties["Kills"].ToString();
+            kvp.Value.PlayerCreepKills.text = player.CustomProperties["CreepKills"].ToString();
+            kvp.Value.PlayerDeaths.text = player.CustomProperties["Deaths"].ToString();
 
-                playerUIStats.PlayerName.text = player.NickName;
-                playerUIStats.PlayerKills.text = player.CustomProperties["Kills"].ToString();
-                playerUIStats.PlayerCreepKills.text = player.CustomProperties["CreepKills"].ToString();
-                playerUIStats.PlayerDeaths.text = player.CustomProperties["Deaths"].ToString();
+            var rect = kvp.Value.GetComponent<RectTransform>();
+            rect.localPosition = new Vector2(0, yPos);
 
-                var rect = playerUIStats.GetComponent<RectTransform>();
-                rect.localPosition = new Vector2(0, yPos);
-                
-                yPos -= 50;
-            }
-            else
-            {
-                Debug.Log("Player not seen on Scoreboard before. Creating new PlayerUIStats obj for it...");
-                var s = Instantiate(m_playerUIStatsObject, m_playerUIStatsObject.transform.position, Quaternion.identity, m_scoreboard.transform);
-                m_playerUIStats.Add(player.ActorNumber, s.GetComponent<PlayerUIStats>());
-                UpdateScoreboard();
-            }
+            yPos -= 50;
         }
     }
 }
